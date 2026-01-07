4ab1af70098d5f1c192374292e4869db73c7128d
yuriyuriyuriuri-ops <yuriyuriyuriuri@gmail.com>
2026-01-07 17:03:34 +0900

    コミット

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "こんにちは、これはRenderで動いてるFlaskアプリです！"
+    return "こんにちは、これはFlaskアプリです！"
 
 @app.route("/a")
 def hello():
     return "これは /a ページです。"
-
-# Renderでは gunicorn が起動するので app.run() は不要
