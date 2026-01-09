98cbad5c8acc799ba702150ef605fe1d7c629fb2
yuriyuriyuriuri-ops <yuriyuriyuriuri@gmail.com>
2026-01-09 12:02:37 +0900

    fix: remove app.run for Railway

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
             <p>こんにちは、これはFlaskアプリです！</p>
             <h1>FlaskでのWebページ</h1>
                 <h2 style="color : orange;">こんにちは</h2>
-                <p><a href="https://my-flask-app-production-bf54.up.railway.app/a">さようなら</a></p>
+                <p><a href="my-flask-app-production-56cf.up.railway.app/a">さようなら</a></p>
             """
 
 @app.route("/a")
@@ -38,4 +38,4 @@ def hello():
             <p>これは /a ページです。</p>
             <h1>FlaskでのWebページ2</h1>
                 <h2 style="color : indigo;">さようなら</h2>
-                <a><a href="https://my-flask-app-production-bf54.up.railway.app/">こんにちは</a></p>"""
+                <a><a href="my-flask-app-production-56cf.up.railway.app/">こんにちは</a></p>"""
